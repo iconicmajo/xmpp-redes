4d7529a1f94e1613a4e4a70973ff2536f2b1b009
iconicmajo <cas181202@uvg.edu.gt>
2021-08-11 18:39:36 -0600

    Update account.py

--- account.py
+++ account.py
@@ -1,12 +1,12 @@
-#Maria Jose Castro - 181202
-#Proyecto Cliente XMPP
-#Archivo del User options
+# Maria Jose Castro - 181202
+# Proyecto Cliente XMPP
+# Archivo del User options
 
-#import ade librerias 
+# import ade librerias
 import xmpp
 
 
-#Creacion de usuario
+# Creacion de usuario
 def createUser(new_user, new_password):
     user = new_user
     password = new_password
@@ -17,6 +17,23 @@ def createUser(new_user, new_password):
     if xmpp.features.register(cli, jid.getDomain(), {'username': jid.getNode(), 'password': password}):
         return True
     else:
-        return False 
+        return False
 
-#iniciar sesion
+# iniciar sesion
+
+
+def login(user, password):
+  userName = input("Usuario (name@alumchat.xyz)>>> ")
+  passWord = input("Contraseña>>> ")
+  user = userName
+  psswrd = passWord
+  print("-----------------")
+  print(user, psswrd)
+  """xmpp = EchoBot(userName, passWord, "1")
+  xmpp.register_plugin('xep_0030') # Service Discovery
+  xmpp.register_plugin('xep_0199') 
+  xmpp.connect()
+  xmpp.process(timeout = 10)"""
+  print("USUARIO LOGGEADO")
+  # xmpp.process(forever=True)
+  # return xmpp
