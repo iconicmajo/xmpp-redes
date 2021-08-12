c16893f008f25c369b5411352eec31ed906e1728
iconicmajo <cas181202@uvg.edu.gt>
2021-08-11 23:30:47 -0600

    Ya funciona registrar usuarios y ya agregamos menu!!

--- account.py
+++ account.py
@@ -7,27 +7,55 @@ import xmpp
 
 
 # Creacion de usuario
-def createUser(new_user, new_password):
-    user = new_user
-    password = new_password
-    jid = xmpp.JID(user)
-    cli = xmpp.Client(jid.getDomain(), debug=[])
-    cli.connect()
+def createUser():
+	print(' ')
+	print('You Choose Create User')
+	new_user = input('username@alumchat.xyz:  ')
+	new_password = input('password:  ')
+	user = new_user
+	password = new_password
+	jid = xmpp.JID(user)
+	cli = xmpp.Client(jid.getDomain(), debug=[])
+	cli.connect()
 
-    if xmpp.features.register(cli, jid.getDomain(), {'username': jid.getNode(), 'password': password}):
-        return True
-    else:
-        return False
+	if xmpp.features.register(cli, jid.getDomain(), {'username': jid.getNode(), 'password': password}):
+		return True
+	else:
+		return False
+
 
 # iniciar sesion
+def login():
+		print(' ')
+		print('You Choose Login')
+		userName = input('user:  ')
+		passWord = input('password: ')
+		user = userName
+		psswrd = passWord
+		#print(user+' '+ psswrd)
+		# xmpp.process(forever=True)
+		# return xmpp
 
+#cerrar sesion
+def logout():
+		print(' ')
+		print('You Choose Logout')
+		#userName = input('user:  ')
+		passWord = input('password: ')
+		user = userName
+		psswrd = passWord
+		#print(user+' '+ psswrd)
+		# xmpp.process(forever=True)
+		# return xmpp
 
-def login(user, password):
-  userName = input("user:  ")
-  passWord = input("password: ")
-  user = userName
-  psswrd = passWord
-  print("-----------------")
-  print(user, psswrd)
-  # xmpp.process(forever=True)
-  # return xmpp
+#Eliminar la cuenta	 
+def deleteAccount():
+		print(' ')
+		print('You Choose Delete Account')
+		userName = input('user:  ')
+		passWord = input('password: ')
+		user = userName
+		psswrd = passWord
+		#print(user+' '+ psswrd)
+		# xmpp.process(forever=True)
+		# return xmpp
--- main.py
+++ main.py
@@ -6,6 +6,65 @@
 import sys 
 import time 
 import ssl
-from slixmpp import ClientXMPP
+from account import *
+#from slixmpp import ClientXMPP
+
+while True:
+    print('***********************')
+    print('       CHAT REDES      ')
+    print('***********************')
+    print('1. Login')
+    print('2. Create User')
+    print('3. Send Private Message')
+    print('4. Send Brodcast Message')
+    print('5. Presence Message')
+    print('6. See contact Info')
+    print('7. Get Notifications')
+    print('8. Send Files')
+    print('9. Logout')
+    print('10. Delete Account')
+    print('')
+    option = input('Please select an option: ')
+
+    if option == '1':
+      print('aun no sirve')
+      login()
+
+    elif option == '2':
+      createUser()
+
+    elif option == '3':
+      print('option 3')
+
+    elif option == '4':
+      print('option 4')
+      
+    elif option == '5':
+      print('option 5')
+
+    elif option == '6':
+      print('option 6')
+
+    elif option == '7':
+      print('option 7')
+
+    elif option == '8':
+      print('option 8')
+
+    elif option == '9':
+      print('option 9')
+      logout()
+
+    elif option == '10':
+      print('option 10')
+      deleteAccount()
+
+
+
+
+
+
+
+
 
 
